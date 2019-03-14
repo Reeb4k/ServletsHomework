70baa1d22b0455dc338185161337ae50f889478f
Ilja Robalkin <ilyaryb@mail.ru>
2019-03-14 19:29:21 +0200

    initial commit

---
+++ src/com/homework/level1/DisplayServlet.java
@@ -0,0 +1,43 @@
+package com.homework.level1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.rmi.ServerException;
+
+@WebServlet(urlPatterns = "/display")
+public class DisplayServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServerException, IOException {
+
+        String name = req.getParameter("name");
+
+        if (name != null){
+
+            resp.setContentType("text/html");
+            PrintWriter out = resp.getWriter();
+            out.print("<html>" +
+                    "<head>" +
+                    "<title> Name entered </title>" +
+                    "</head>" +
+                    "<body>" +
+                    "<h1 align= \"center\"> You look terrific today!</h1>" +
+                    "</body>" +
+                    "</html>");
+
+        } else {
+            resp.sendRedirect("/data");
+        }
+
+    }
+}
